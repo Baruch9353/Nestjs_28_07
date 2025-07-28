40ff5ccd86f00ee22f4886aad357df37f6827d4b
baruch9353 <baruchminzberg@gmail.com>
2025-07-28 18:05:25 +0300

    First stage completed successfully

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,50 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { UsersService } from '../users/users.service';
+import { RegisterDto } from './dto/register.dto';
+import { LoginDto } from './dto/login.dto';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(
+    private jwtService: JwtService,
+    private usersService: UsersService
+  ) {}
+
+  register(registerDto: RegisterDto) {
+    const existingUser = this.usersService.findByUsername(registerDto.username);
+    if (existingUser) {
+        throw new UnauthorizedException('Username already exists');
+    }
+    const newUser = this.usersService.addUser(registerDto);
+    return {
+        message: 'User registered successfully',
+        user: newUser,
+    };
+  }
+
+
+  login(loginDto: LoginDto) {
+    const { username, password } = loginDto;
+    const user = this.usersService.findByUsername(username);
+
+    if (!user || user.password !== password) {
+      throw new UnauthorizedException('Invalid credentials');
+    }
+    const token = this.generateToken(user);
+
+    return {
+      access_token: token,
+    };
+  }
+
+  private generateToken(user: any): string {
+    const payload = {
+      sub: user.id,
+      username: user.username,
+      role: user.role,
+    };
+
+    return this.jwtService.sign(payload);
+  }
+}
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,46 @@
 import { Injectable } from '@nestjs/common';
+import { RegisterDto } from '../auth/dto/register.dto';
+
+export type User = {
+  id: number;
+  username: string;
+  password: string;
+  role: 'soldier' | 'commander';
+};
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  private users: User[] = [
+    {
+      id: 1,
+      username: 'bar',
+      password: '12',
+      role: 'commander',
+    },
+    {
+      id: 2,
+      username: 'avi',
+      password: 'ab',
+      role: 'soldier',
+    },
+  ];
+
+  findByUsername(username: string): User | undefined {
+    return this.users.find((user) => user.username === username);
+  }
+
+ addUser(dto: RegisterDto) {
+    const newUser = {
+        id: this.users.length + 1,
+        username: dto.username,
+        password: dto.password,
+        role: dto.role,
+    };
+
+    this.users.push(newUser);
+    return newUser;
+  }
+    getAllUsers(): User[] {
+     return this.users;
+  }
+}
