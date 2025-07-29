c0d70f69759a14ad4056b2725d029278657f4d3f
baruch9353 <baruchminzberg@gmail.com>
2025-07-29 09:19:39 +0300

    Deleted unnecessary files

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import {config} from 'dotenv';
+import { config } from 'dotenv';
+
 config();
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT || 3000, () => {
-    console.log(`Server running on port ${process.env.PORT}`)})
+  try {
+    const app = await NestFactory.create(AppModule);
+    const PORT = process.env.PORT || 3000;
+    await app.listen(PORT);
+    console.log(`Server running on port ${PORT}`);
+  } catch (err) {
+    console.error('Failed to start server:', err);
+  }
 }
 bootstrap();
