cae1f504a3f02d82bf6ea282eaada0e5f059a047
baruch9353 <baruchminzberg@gmail.com>
2025-07-30 18:11:26 +0300

    end_30_07

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,8 +4,18 @@ import { UsersModule } from './users/users.module';
 import { ShiftsModule } from './shifts/shifts.module';
 import { AssignmentsModule } from './assignments/assignments.module';
 import { ConfigModule } from '@nestjs/config';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
-  imports: [AuthModule, UsersModule, ShiftsModule, AssignmentsModule, ConfigModule.forRoot({ isGlobal: true })]
+
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    JwtModule.register({
+      global: true,
+      secret: process.env.JWT_SECRET,
+      signOptions: { expiresIn: '99h' },
+    }),
+    AuthModule, UsersModule, ShiftsModule, AssignmentsModule,],
 })
 export class AppModule {}
+  
